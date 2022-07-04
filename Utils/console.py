add9631f0b774a1593de052af934c5a99dca3dc0
Serkhani <deviper03@gmail.com>
2022-07-04 11:59:40 +0100

    adding utils

---
+++ Utils/console.py
@@ -0,0 +1,29 @@
+"""
+Okay, have to admit. This code is from Elubum's RedditVideoMakerBot project
+"""
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.padding import Padding
+from rich.panel import Panel
+from rich.text import Text
+
+console = Console()
+
+
+def print_markdown(text):
+    """Prints a rich info message. Support Markdown syntax."""
+
+    md = Padding(Markdown(text), 2)
+    console.print(md)
+
+
+def print_step(text):
+    """Prints a rich info message."""
+
+    panel = Panel(Text(text, justify="left"))
+    console.print(panel)
+
+
+def print_substep(text, style=""):
+    """Prints a rich info message without the panelling."""
+    console.print(text, style=style)
